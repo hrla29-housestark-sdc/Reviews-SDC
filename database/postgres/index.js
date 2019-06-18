05a3e57f22b18805dcebef198f15e382fa4b5014
Jeff Cho <jeffcho0227@gmail.com>
2019-06-17 17:14:10 -0700

    [refactor] updated ec2 database address

--- database/postgres/index.js
+++ database/postgres/index.js
@@ -2,7 +2,7 @@ const { Pool } = require('pg');
 const format = require('pg-format');
 
 const config = {
-  host: 'ec2-3-16-90-106.us-east-2.compute.amazonaws.com',
+  host: 'ec2-3-16-124-49.us-east-2.compute.amazonaws.com',
   user: 'ubuntu',
   password: 'password',
   database: 'reviews',
